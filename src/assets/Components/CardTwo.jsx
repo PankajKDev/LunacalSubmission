61ee3547ac65e13143de899d9639d5aa23578a1c
PankajKDev <pankajk.dev0000@gmail.com>
2024-09-05 11:49:58 +0530

    added functionality to addImages and map them from FileList

--- src/assets/Components/CardTwo.jsx
+++ src/assets/Components/CardTwo.jsx
@@ -1,20 +1,54 @@
 
+import { useState } from "react"
 import Breaker from "./Breaker"
 import Toolbar from "./Toolbar"
 
 const CardTwo = () => {
+
+const [images,setImages]=useState([])//holds images
+
+const [previews,setPreviews]=useState([])//holds array of URl's
+
+const handleImageChange=(event)=>{
+  const files=Array.from(event.target.files) //converting uploaded images into array
+  //limiting images
+  if(images.length+files.length>3){
+   previews.shift()
+   images.shift()
+  }
+  const newImages=[]//holds new Images
+  const newPreviews=[] //holds new data URLs
+
+  files.forEach((file)=>{
+    const reader=new FileReader()
+    reader.onload=()=>{
+      newImages.push(file);
+      newPreviews.push(reader.result)
+      //pushing file and DataURLs to newImages and newPreviews
+
+//Updating state after reading Images
+if(newImages.length===files.length){
+  setImages((prevImages)=>[...prevImages,...newImages])
+  setPreviews((prevPreviews)=>[...prevPreviews,...newPreviews])
+}//appending new Images with old ones
+
+    }
+    reader.readAsDataURL(file)
+  })
+
+}
+
+
   return (
     <>
-    
-
     <div className="w-[calc(720px-3rem)] md:w-[720px]  h-[316px] flex flex-col justify-center card">
           <div className="w-full flex justify-center">
             <div className="card-header-two">
               <div>
-                <button className="w-[149px] h-[62px]  rounded-[20px] font-Poppins bg-black text-white hover:bg-white hover:text-black">Gallery</button>
+                <button className="w-[149px] h-[62px]  rounded-[20px] font-Poppins bg-black text-white">Gallery</button>
               </div>
               <div className="gap-5 flex">
-                <input className="hidden" type="file" multiple  id="inputImage" />
+                <input className="hidden" accept="image/" type="file" id="inputImage"  onChange={handleImageChange} />
                 <label className="plusButton" htmlFor="inputImage">        
                   +&nbsp; ADD IMAGE         
                 </label>
@@ -31,9 +65,13 @@ const CardTwo = () => {
           <div className="bg-full h-full flex"> 
    <Toolbar tid="card"/>
    <div className="mt-[28px] flex gap-3 ">
-    <img className="imageCard" src="/building.svg" alt="" />
-    <img className="imageCard" src="/building.svg" alt="" />
-    <img className="imageCard" src="/building.svg" alt="" />
+   {previews.length > 0 ? (
+              previews.map((preview, index) => (
+                <img key={index} accept="image/*" src={preview} alt={`Uploaded Image Preview ${index + 1}`} className="imageCard" />
+              ))
+            ) : (
+              <div className="w-[650px] rounded-md inset-3 h-[180px] flex justify-center items-center text-[#969696] font-Jakarta text-2xl  border border-gray-600">No images uploaded yet.</div>
+            )}
    </div>
           </div>
     </div>
